b05c0634f838a8f0b0e6cbd9cd1474d7405d4d54
zafkiel <phamdinhthai2005@gmail.com>
2026-06-07 21:28:48 +0700

    backend/user.model

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const userRoutes = require('./src/routes/users.route');
 
 const app = express();
 const port = 3000;
 
+app.use('/api/users', userRoutes);
 app.get('/api', (req, res) => {
   res.send('Hello World!');
 });
